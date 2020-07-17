48bd690fbde5745ede9a025809293e3955e8d226
近藤元気 <genki7onepiece@gmail.com>
2020-07-17 20:05:00 +0900

    Initial Commit

---
+++ FutureCreator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FutureCreator
+//
+//  Created by 近藤元気 on 2020/07/17.
+//  Copyright © 2020 genkikondo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
